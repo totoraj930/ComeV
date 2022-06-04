16cc007604ff213d0d37943facd36da9b7bddf8b
Reona Oshima(totoraj) <totoradesu-contact@yahoo.co.jp>
2022-06-04 23:07:52 +0900

    ちゃんとコピー出来てなかった

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useReducer } from 'react';
 import { LiveView } from './components/LiveView';
-import { AppConfigContext, appConfigReducer, defaultConfig } from './context/config';
+import { AppConfigContext, appConfigReducer, copyConfig, defaultConfig } from './context/config';
 import { LiveChatProvider } from './context/liveChat';
 import './styles/main.scss';
 function App() {
-  const [state, dispatch] = useReducer(appConfigReducer, {...defaultConfig});
+  const [state, dispatch] = useReducer(appConfigReducer, copyConfig(defaultConfig));
   return (
     <AppConfigContext.Provider value={{state, dispatch}}>
       <LiveChatProvider>
