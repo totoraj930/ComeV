3145e511ec08d6d6006dbf00d0eb58b594d91e9f
Reona Oshima(totoraj) <totoraj930@yahoo.co.jp>
2022-06-08 19:34:11 +0900

    いまどきな書き方に変えた

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
