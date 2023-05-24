d74268c0d21b25cfef81e8b16bc230efc1f30d06
Reona Oshima(totoraj) <totoraj930@yahoo.co.jp>
2023-05-24 15:33:05 +0900

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { createSignal } from 'solid-js';
+import logo from './assets/logo.svg';
+import { invoke } from '@tauri-apps/api/tauri';
+import './App.css';
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  const [greetMsg, setGreetMsg] = createSignal('');
+  const [name, setName] = createSignal('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
+    setGreetMsg(await invoke('greet', { name: name() }));
   }
 
   return (
